6d017c08bb7053f01e73e013fd2d35d5e3d86aa0
SirojiddinMirzaqodirov <perfectworldfrom2021@gmail.com>
2023-09-19 15:03:14 +0500

    New editing

---
+++ backend/frontend/src/components/app/app.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from '../header/header'
+import Experience from '../experience/experience'
+import Education from '../education/education'
+import Skills from '../skills/skills'
+import Contactme from '../contactme/contactme'
+
+
+const App = () => {
+  return (
+    <div><Header /></div>
+    <div><Experience /></div>
+    <div><Education /></div>
+    <div><Skills /></div>
+    <div><Contactme /></div>
+  )
+}
+
+export default App
